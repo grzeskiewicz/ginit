be07c349af7e73737fef5cf3858cd14afe6c9e2e
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2018-09-07 14:15:00 +0200

    Edit

--- index.js
+++ index.js
@@ -21,12 +21,47 @@ if (files.directoryExists('.git')) {
 }
 
 
-const run = async() => {
+const getGithubToken = async() => {
+    // Fetch token from config store
     let token = github.getStoredGithubToken();
-    if (!token) {
-        await github.setGithubCredentials();
-        token = await github.registerNewToken();
+    if (token) {
+        return token;
     }
-    console.log(token);
+    // No token found, use credentials to access GitHub account
+    await github.setGithubCredentials();
+    // register new token
+    token = await github.registerNewToken();
+    return token;
 }
+
+const run = async() => {
+    try {
+        // Retrieve & Set Authentication Token
+        const token = await getGithubToken();
+        github.githubAuth(token);
+        // Create remote repository
+        const url = await repo.createRemoteRepo();
+        // Create .gitignore file
+        await repo.createGitignore();
+        // Set up local repository and push to remote
+        const done = await repo.setupRepo(url);
+        if (done) {
+            console.log(chalk.green('All done!'));
+        }
+    } catch (err) {
+        if (err) {
+            switch (err.code) {
+                case 401:
+                    console.log(chalk.red('Couldn\'t log you in. Please provide correct credentials/token.'));
+                    break;
+                case 422:
+                    console.log(chalk.red('There already exists a remote repository with the same name'));
+                    break;
+                default:
+                    console.log(err);
+            }
+        }
+    }
+}
+
 run();
